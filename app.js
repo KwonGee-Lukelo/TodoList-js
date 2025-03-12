d23eaf9c6773fc26423de01590b5b65bda2b654d
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-12 19:58:45 +0100

    Creation d'un element i a qui on ajouter de classe pour avoir notre icon de suppression et aussi on ajoute un IF pour verifier que si notre champ de saisie est vide qu'on ajoute rien sur la divViewData

--- app.js
+++ app.js
@@ -7,16 +7,32 @@ addBtn.addEventListener("click", AddData);
 function AddData(e) {
   e.preventDefault();
 
-  //CREATION D'UN ELEMENT DIV
-  const divElement = document.createElement("div");
-  divElement.classList.add("div__element");
-  divViewData.appendChild(divElement);
+  if (data.value !== "") {
+    //CREATION D'UN ELEMENT DIV
+    const divElement = document.createElement("div");
+    divElement.classList.add("div__element");
+    divViewData.appendChild(divElement);
 
-  //CREATION D'UN ELEMENT LI
-  const liElement = document.createElement("li");
-  divElement.appendChild(liElement);
+    //CREATION D'UN ELEMENT LI
+    const liElement = document.createElement("li");
+    liElement.classList.add("li__element");
+    divElement.appendChild(liElement);
 
-  let dataOfInput = data.value.trim();
-  liElement.innerText = dataOfInput;
-  console.log(dataOfInput);
+    //AJOUT DE l'ICON SUPPRIMER DANS LA DIV ELEMENT
+    const deleteBtn = document.createElement("i");
+    deleteBtn.classList.add("ri-delete-bin-6-line");
+    deleteBtn.style.color = "white";
+    deleteBtn.style.backgroundColor = "#009aff";
+    deleteBtn.style.padding = "0.5em";
+    divElement.appendChild(deleteBtn);
+
+    // AJOUT DE L'ÉVÉNEMENT DE SUPPRESSION
+    /* deleteBtn.addEventListener("click", () => {
+        divElement.remove();
+      });*/
+    let dataOfInput = data.value.trim();
+    liElement.innerText = dataOfInput;
+
+    data.value = "";
+  }
 }
