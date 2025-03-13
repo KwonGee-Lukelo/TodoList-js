65b43312bd496ae52e5a5fcad64a7f205f9a46d8
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-13 09:49:33 +0100

    Implementation de la fonctionalité SUPPRIMER

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const addBtn = document.querySelector(".icon__add");
 const divViewData = document.querySelector("#listData");
 
 addBtn.addEventListener("click", AddData);
+divViewData.addEventListener(
+  "click",
+  deleteData
+); /*cette fonction va se déclenché lorqu'on 
+va clique sur un élément de cette div*/
 
 function AddData(e) {
   e.preventDefault();
@@ -27,12 +32,24 @@ function AddData(e) {
     divElement.appendChild(deleteBtn);
 
     // AJOUT DE L'ÉVÉNEMENT DE SUPPRESSION
-    /* deleteBtn.addEventListener("click", () => {
-        divElement.remove();
-      });*/
+    /*deleteBtn.addEventListener("click", () => {
+      divElement.remove();
+    });*/
+
     let dataOfInput = data.value.trim();
     liElement.innerText = dataOfInput;
 
     data.value = "";
+  } else {
+    alert("VEUILLEZ AJOUTER UNE TACHE");
   }
+}
+
+function deleteData(e) {
+  /*dans notre projet nous trouvons un élément qui 
+  contient cette classe car l'evenement va se declanché sur elle*/
+  const deleteIcon = e.target.classList.contains("ri-delete-bin-6-line");
+  const divRemove = e.target.parentElement; //Puis on cible l'élément parent
+
+  divRemove.remove(); //On supprime l'élément parent
 }
