126cae63a4b04f0caf96bc870c0af8b38cee1ec8
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-13 14:44:23 +0100

    Implementation de la fonction checkingIconClick qui verifie quelle icône a été cliquée et appelle la fonction appropriée

--- app.js
+++ app.js
@@ -3,46 +3,43 @@ const addBtn = document.querySelector(".icon__add");
 const divViewData = document.querySelector("#listData");
 
 addBtn.addEventListener("click", AddData);
-divViewData.addEventListener(
-  "click",
-  deleteData
-); /*cette fonction va se déclenché lorqu'on 
-va clique sur un élément de cette div*/
+divViewData.addEventListener("click", checkingIconClick);
 
 function AddData(e) {
   e.preventDefault();
 
   if (data.value !== "") {
-    //CREATION D'UN ELEMENT DIV
+    // CREATION D'UN ELEMENT DIV
     const divElement = document.createElement("div");
     divElement.classList.add("div__element");
     divViewData.appendChild(divElement);
 
-    //CREATION D'UN ELEMENT LI
+    // CREATION D'UN ELEMENT LI
     const liElement = document.createElement("li");
     liElement.classList.add("li__element");
     divElement.appendChild(liElement);
 
-    //AJOUT DE l'ICON SUPPRIMER DANS LA DIV ELEMENT
+    // AJOUT DE L'ICON SUPPRIMER DANS LA DIV ELEMENT
     const deleteBtn = document.createElement("i");
-    deleteBtn.classList.add("ri-delete-bin-6-line");
+    deleteBtn.classList.add("ri-delete-bin-6-line", "delete");
     deleteBtn.style.color = "white";
     deleteBtn.style.backgroundColor = "#009aff";
     deleteBtn.style.padding = "0.5em";
-    divElement.appendChild(deleteBtn);
 
-    //AJOUT DE l'ICON COMPLETER DANS LA DIV ELEMENT
+    // AJOUT DE L'ICON COMPLETER DANS LA DIV ELEMENT
     const completeBtn = document.createElement("i");
-    completeBtn.classList.add("ri-check-double-fill");
+    completeBtn.classList.add("ri-check-double-fill", "completed");
     completeBtn.style.color = "white";
-    completeBtn.style.backgroundColor = "#009aff";
+    completeBtn.style.backgroundColor = "#52fbdf";
     completeBtn.style.padding = "0.5em";
-    divElement.appendChild(completeBtn);
+    completeBtn.style.marginLeft = ".5em";
 
-    // AJOUT DE L'ÉVÉNEMENT DE SUPPRESSION
-    /*deleteBtn.addEventListener("click", () => {
-      divElement.remove();
-    });*/
+    // DIV QUI CONTIENDRA TOUT LES ELEMENT I(icone)
+    const divAllIcon = document.createElement("div");
+    divAllIcon.classList.add("div__icons");
+    divAllIcon.appendChild(deleteBtn);
+    divAllIcon.appendChild(completeBtn);
+    divElement.appendChild(divAllIcon);
 
     let dataOfInput = data.value.trim();
     liElement.innerText = dataOfInput;
@@ -53,11 +50,27 @@ function AddData(e) {
   }
 }
 
-function deleteData(e) {
-  /*dans notre projet nous trouvons un élément qui 
-  contient cette classe car l'evenement va se declanché sur elle*/
-  const deleteIcon = e.target.classList.contains("ri-delete-bin-6-line");
-  const divRemove = e.target.parentElement; //Puis on cible l'élément parent
+/*Cette fonction sert à gérer les clics sur les icônes de suppression et de complete
+Elle vérifie quelle icône a été cliquée et appelle la fonction appropriée 
+(deleteData ou completeData) en fonction de la classe de l'icône cliquée*/
+function checkingIconClick(e) {
+  if (e.target.classList.contains("ri-delete-bin-6-line")) {
+    deleteData(e);
+  } else if (e.target.classList.contains("ri-check-double-fill")) {
+    completeData(e);
+  }
+}
 
-  divRemove.remove(); //On supprime l'élément parent
+function deleteData(e) {
+  const divParent = e.target.parentElement; // Puis on cible l'élément parent, son parent direct
+  const divRemove = divParent.parentElement; // ici à partir du parent direct, nous ciblons le parent du parent direct
+
+  divRemove.remove(); // On supprime l'élément Parent du parent direct
+}
+
+function completeData(e) {
+  const divParent = e.target.parentElement; // Puis on cible l'élément parent, son parent direct
+  const liElement = divParent.previousElementSibling; // ici à partir du parent direct, nous ciblons l'élément li précédent
+
+  liElement.classList.toggle("completed__text"); // Ajoute ou supprime la classe pour marquer comme complété
 }
